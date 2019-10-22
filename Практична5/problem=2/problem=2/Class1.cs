301848290e4439e3c8c4523cbf01a2d07bb37e9f
MIchaell22 <55326207+MIchaell22@users.noreply.github.com>
2019-10-22 19:17:03 +0300

    1

--- Практична5/problem=2/problem=2/Class1.cs
+++ Практична5/problem=2/problem=2/Class1.cs
@@ -4,23 +4,101 @@ using System.Text;
 
 namespace problem_2
 {
-    class polyhedron
+    class Polyhedron
     {
-        public float L { get; set; }
-        public float W { get; set; }
-        public float H { get; set; }
+        private float l;
+        private float w;
+        private float h;
 
+    public  Polyhedron(float l,float w,float h )
+
+        {
+            this.L = l;
+            this.W = w;
+            this.H = h;
+        }
+        public float L {
+            get
+            {
+                return this.L;
+            }
+
+            set
+            {
+
+
+                if (value < 0 || value == 0)
+                {
+                    throw new ArgumentException("Width  cannot  be  zero  or  negative.");
+
+                }
+
+
+               
+
+
+            }
+
+        }
+
+        public float W
+        {
+            get
+            {
+                return this.W;
+            }
+
+            set
+            {
+
+
+                if (value < 0 || value == 0)
+                {
+                    throw new ArgumentException("Width  cannot  be  zero  or  negative.");
+
+                }
+
+
+               
+
+
+            }
+        }
+        public float H
+        {
+            get
+            {
+                return this.H;
+            }
+
+            set
+            {
+
+
+                if (value < 0 || value == 0)
+                {
+                    throw new ArgumentException("Width  cannot  be  zero  or  negative.");
+
+                }
+
+
+              
+
+
+            }
+        }
+        
         public static void getSurfaceArea(float L, float W, float H)
         {
-            Console.WriteLine(L * W * H);
+            Console.Write("SurfaceArea = "); Console.WriteLine(   L * W * H);
         }
         public static void getLateralSurfaceArea(float L, float W, float H)
         {
-            Console.WriteLine(2 * L * H + 2 * W * H);
+            Console.Write("LateralSurfaceArea = ");  Console.WriteLine(  2 * L * H + 2 * W * H);
         }
         public static void getVolume(float L, float W, float H)
         {
-            Console.WriteLine(2 * L * W + 2 * L * H + 2 * W * H);
+            Console.Write("Volume = "); Console.WriteLine( 2 * L * W + 2 * L * H + 2 * W * H);
         }
     }
 }
--- Практична5/problem=2/problem=2/Program.cs
+++ Практична5/problem=2/problem=2/Program.cs
@@ -6,28 +6,34 @@ namespace problem_2
     {
         static void Main(string[] args)
         {
-
-            float count = float.Parse(Console.ReadLine());
-
-
-
-            for (int i = 0; i < count; i++)
+            try
             {
-                var tokens = Console.ReadLine().Split();
-                float L = float.Parse(tokens[0]);
-                float W = float.Parse(tokens[1]);
-                float H = float.Parse(tokens[2]);
-                if ((float.Parse(tokens[0]) == 0) || (float.Parse(tokens[0]) < 0) || (float.Parse(tokens[1]) == 0) || (float.Parse(tokens[1]) < 0) || (float.Parse(tokens[2]) == 0) || (float.Parse(tokens[2]) < 0))
+                float count = float.Parse(Console.ReadLine());
+
+
+
+                for (int i = 0; i < count; i++)
                 {
-                    Console.WriteLine("Width  cannot  be  zero  or  negative.");
+                    var tokens = Console.ReadLine().Split();
+                    float L = float.Parse(tokens[0]);
+                    float W = float.Parse(tokens[1]);
+                    float H = float.Parse(tokens[2]);
+
+                    Polyhedron polyhedron = new Polyhedron( L, W, H);
+
+                   Polyhedron.getSurfaceArea(L,W,H);
+                    Polyhedron.getLateralSurfaceArea(L, W, H);
+                     Polyhedron.getVolume(L, W, H);
                 }
-               else
-                {
-                    Console.Write(" Surface  Area = "); polyhedron.getSurfaceArea(float.Parse(tokens[0]), float.Parse(tokens[1]), float.Parse(tokens[2]));
-                    Console.Write("Lateral Surface Area =  "); polyhedron.getLateralSurfaceArea(float.Parse(tokens[0]), float.Parse(tokens[1]), float.Parse(tokens[2]));
-                    Console.Write(" Volume = "); polyhedron.getVolume(float.Parse(tokens[0]), float.Parse(tokens[1]), float.Parse(tokens[2]));
-                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+
+            }
+
             }
         }
     }
-}
+
